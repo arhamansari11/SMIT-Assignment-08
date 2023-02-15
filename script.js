571fb1c900720f6a4bf4ff0bb116127077cd5d2a
arhamansari11 <124850772+arhamansari11@users.noreply.github.com>
2023-02-15 16:04:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+var admin;
+var name = "Umair Azmat";
+
+admin = name;
+
+alert(admin); // Outputs "Umair Azmat"
+
+
+var earth = "Earth";
+
+
+var currentVisitor = "Umair Azmat";
+
+var favDrink = "coffee";
+
+var favNum = 7;
+
+function myFavs() {
+    var favDrink = "Iced coffee";
+    var favNum = 7;
+    
+    console.log("My favorite drink is " + favDrink);
+    console.log("My favorite number is " + favNum);
+  }
+  
+  myFavs(); // Invoke the function
+  
+
+var person = {
+    name: "Umair",
+    age: 20
+  };
+
+
+  var num1 = 5;
+var num2 = 10;
+var sum = num1 + num2;
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+
+
+
+var num1 = 10;
+var num2 = 2;
+var result = num1 / num2;
+
+console.log("The result of dividing " + num1 + " by " + num2 + " is " + result + ".");
+
+
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+var result = sum / 5;
+
+console.log("The result of adding " + num1 + " and " + num2 + " and dividing the sum by 5 is " + result + ".");
+
+
+var tempF = 50;
+var tempC = (tempF - 32) * 5 / 9;
+
+console.log("The temperature of " + tempF + " degrees Fahrenheit is equivalent to " + tempC + " degrees Celsius.");
+
+
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
+var sum = num1 + num2;
+
+alert("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+
+
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
+var difference = num1 - num2;
+
+console.log("The difference between " + num1 + " and " + num2 + " is " + difference + ".");
+
+
+
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
+var product = num1 * num2;
+
+alert("The product of " + num1 + " and " + num2 + " is " + product + ".");
+
+
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
+var quotient = num1 / num2;
+
+console.log("The quotient of " + num1 + " and " + num2 + " is " + quotient + ".");
+
+
+var num1 = parseInt(prompt("Enter the first number:"));
+var num2 = parseInt(prompt("Enter the second number:"));
+var remainder = num1 % num2;
+
+alert("The remainder of " + num1 + " and " + num2 + " is " + remainder + ".");
+
+
+  
